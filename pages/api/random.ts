e3120788b3b9101a8c069fabc7e931567ea4e2c9
kashif <73710039+KashifBrula@users.noreply.github.com>
2024-06-03 16:23:41 +0500

    Billboard Component Ui & Api Hooks

--- pages/api/random.ts
+++ pages/api/random.ts
@@ -3,12 +3,12 @@ import { NextApiRequest, NextApiResponse } from "next";
 import prismadb from '@/lib/primsadb';
 import serverAuth from "@/lib/serverAuth";
 
-export default async function hanlder( req: NextApiRequest, res: NextApiResponse) {
-    if( req.method !== "GET" ) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== "GET") {
         return res.status(405).end();
     }
 
-    try{
+    try {
         await serverAuth(req);
 
         const movieCount = await prismadb.movie.count();
@@ -20,10 +20,8 @@ export default async function hanlder( req: NextApiRequest, res: NextApiResponse
         })
 
         return res.status(200).json(randomMovies?.[0]);
-    }
-    catch (error) {
+    } catch (error) {
         console.log(error);
         return res.status(400).end();
     }
-    
 }
